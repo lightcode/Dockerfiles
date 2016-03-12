e41b0ef59a54ff28c501d410e5db3ef054fb9334
lightcode <lightcode@gmx.com>
2016-03-12 18:33:08 +0100

    Add http/php5 base image

---
+++ http-php5/boot-scripts.d/00-php5-fmp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php5/fpm/php-fpm.conf
+
+sed -i -e 's/^pm.max_children\s*=.*$/pm.max_children = 10/' \
+  -e 's/^pm.max_spare_servers\s*=.*$/pm.max_spare_servers = 6/' \
+  /etc/php5/fpm/pool.d/www.conf

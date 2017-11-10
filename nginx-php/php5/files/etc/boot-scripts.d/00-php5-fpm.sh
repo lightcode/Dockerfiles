8402eeef03d8e9f4dd6116ba24389786cac96abe
lightcode <lightcode@users.noreply.github.com>
2017-11-10 23:14:52 +0100

    Remove some Dockerfiles from this repo

--- nginx-php/php5/files/etc/boot-scripts.d/00-php5-fpm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euf -o pipefail
-
-sed -i 's/;*daemonize\s*=.*/daemonize = no/g' /etc/php5/fpm/php-fpm.conf
-
-sed -i -e 's/^pm.max_children\s*=.*$/pm.max_children = 10/' \
-  -e 's/^pm.max_spare_servers\s*=.*$/pm.max_spare_servers = 6/' \
-  /etc/php5/fpm/pool.d/www.conf
